294a2d9537e6a9d52df8e5f085ef4aeb3328333b
HanThangdev <thangdtkma@gmail.com>
2023-11-04 00:07:58 +0700

    feat/change-logo

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: 'Home - Simple',
+  title: 'ChatFly - Home',
   description: 'Page description',
 }
 
---
+++ assets/images/index.ts
@@ -0,0 +1,25 @@
+import backgroundImg from "./background.png";
+import iconFooterImg from "./icon-footer.png";
+import imageIntegraImg from "./image-integra.png";
+import imageIntegraImgJp from "./image-integra-jp.png";
+import videoImg from "./video.png";
+import logoDarkImg from "./logo_dark.png";
+import logoLightImg from "./logo_light.png";
+import buttonPriceImg from "./button-price.png";
+import backgroundDifferent from "./bg-different.png";
+import japanLang from "./ja.png";
+import englishLang from "./en.png";
+
+export {
+  logoDarkImg,
+  logoLightImg,
+  backgroundImg,
+  iconFooterImg,
+  imageIntegraImg,
+  imageIntegraImgJp,
+  videoImg,
+  buttonPriceImg,
+  backgroundDifferent,
+  japanLang,
+  englishLang,
+};
